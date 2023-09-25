3704ad2abd712d31d140c71c960648be414d0a26
Mariia Ovs <maruw@ya.ru>
2023-09-25 13:00:07 +0200

    made one page with date, time and date+time buttons

---
+++ app.js
@@ -0,0 +1,39 @@
+const output = document.getElementById("output");
+
+const fullBtn = document.getElementById("full");
+
+const dateBtn = document.getElementById("date");
+
+const timeBtn = document.getElementById("time");
+
+let btnIsOn = false;
+
+setInterval(function () {
+
+    const now = new Date();
+    if (!btnIsOn) {
+        output.innerHTML = now.toLocaleString();
+    } else output.innerHTML = formatTime(now);
+
+}, 1000);
+
+fullBtn.onclick = () => {
+    btnIsOn = true;
+    formatTime = function (time) {
+        return time.toLocaleString();
+    }
+}
+
+dateBtn.onclick = () => {
+    btnIsOn = true;
+    formatTime = function (time) {
+        return time.toLocaleDateString();
+    }
+}
+
+timeBtn.onclick = () => {
+    btnIsOn = true;
+    formatTime = function (time) {
+        return time.toLocaleTimeString();
+    }
+}
